9c5ae1fed12025fc934ecf61bd3b227672c83a9f
zhangtengyue <zty1160051490@163.com>
2020-03-27 19:47:01 +0800

    make-up store

--- src/components/vue-learning/store.vue
+++ src/components/vue-learning/store.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
-    <div>
+    <div class="box">
       count in store: {{$store.state.count}}
+    </div>
+    <div class="box">
+      getter: {{gets}}
+    </div>
+    <div>
+      <el-button type="primary" @click="add">添加</el-button>
+      <el-button type="primary" @click="addNum">+5</el-button>
+      <el-button type="primary" @click="sAdd">异步添加</el-button>
     </div>
   </div>
 </template>
@@ -11,18 +19,35 @@ export default {
   name: "Index",
   data() {
     return {
-
+    
     }
   },
   mounted() {
-
+    console.log('store = ', this.$store)
+  },
+  computed: {
+    gets() {
+      return this.$store.getters.getCount
+    }
   },
   methods: {
-
+    add() {
+      this.$store.commit('addCount')
+    },
+    addNum() {
+      this.$store.commit('addSome', 5)
+    },
+    sAdd() {
+      setTimeout(() => {
+        this.$store.dispatch('add')
+      }, 1000)
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.box {
+  margin: 10px auto;
+}
 </style>
